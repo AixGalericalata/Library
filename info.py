afa7afc32e84a4e933ab80f96e7946ab5a67ea66
Nesya <angerasimov04@outlook.com>
2020-11-23 19:54:49 +0300

    third drop

--- info.py
+++ info.py
@@ -15,5 +15,34 @@ class Info(QWidget):
         self.initUI(id)
 
     def initUI(self, id):
-
         uic.loadUi('details.ui', self)
+
+        con = sqlite3.connect('library.db')
+        cur = con.cursor()
+
+        result = cur.execute("""
+        SELECT * FROM library
+        WHERE id = ?
+        """, (id,)).fetchone()
+
+        self.name.setText(str(result[1]))
+        self.year.setText(str(result[3]))
+
+        author_result = cur.execute("""
+        SELECT name FROM authors WHERE id = ?
+        """, (result[2],)).fetchone()
+
+        self.author.setText(str(author_result[0]))
+
+        genre_result = cur.execute("""
+        SELECT name FROM genres WHERE id = ?
+        """, (result[4],)).fetchone()
+
+        self.genre.setText(str(genre_result[0]))
+        pixmap = QPixmap(f'images/{result[5]}')
+        if pixmap.isNull():
+            pixmap = QPixmap('images/None.png')
+        pixmap = pixmap.scaled(self.picture.size(), QtCore.Qt.KeepAspectRatio,
+                               transformMode=QtCore.Qt.SmoothTransformation)
+        self.picture.setAlignment(QtCore.Qt.AlignCenter)
+        self.picture.setPixmap(pixmap)
